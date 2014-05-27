4fd687a25e73a1ea9522e0656c3a6f63cf0be04b
eryk <xuqi86@gmail.com>
2014-05-27 23:40:25 +0800

    修改完成数据方式

--- src/main/java/com/zhaijiong/stock/SimpleFetcher.java
+++ src/main/java/com/zhaijiong/stock/SimpleFetcher.java
@@ -2,10 +2,8 @@ package com.zhaijiong.stock;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Stopwatch;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Queues;
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -22,7 +20,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * Created by xuqi.xq on 2014/5/26.
  */
-public class SimpleFetcher{
+public class SimpleFetcher {
     private static final String BASEDIR = "e:/stock/";
     private static final String BASEURL = "http://basic.10jqka.com.cn/%s/xls/%s";
     private static final String[] files =
@@ -31,13 +29,13 @@ public class SimpleFetcher{
                     "cashreport.xls"}; // 现金流量表
 
     private ExecutorService executor;
-    private ConcurrentLinkedQueue<String> failTasks;
-    private CountDownLatch latch;
+    private ConcurrentLinkedQueue<String> tasks;
     private AtomicInteger successed = new AtomicInteger(0);
+    private Integer totalTask = 0;
 
-    public void init(){
+    public void init() {
         File file = new File(BASEDIR);
-        if(file.exists()){
+        if (file.exists()) {
             file.delete();
         }
         try {
@@ -46,25 +44,20 @@ public class SimpleFetcher{
             System.out.println("创建文件失败");
         }
 
-        executor = Executors.newFixedThreadPool(50);
-        failTasks = Queues.newConcurrentLinkedQueue();
-
+        executor = Executors.newFixedThreadPool(20);
+        tasks = Queues.newConcurrentLinkedQueue();
     }
 
-    public void downloadStockFile(){
+    public void downloadStockFile() {
         try {
-            Map<String,String> stocks = stockList();
-            latch = new CountDownLatch(stocks.size());
+            Collection<String> values = stockList().values();
+            totalTask = values.size();
+            tasks.addAll(values);
 
-            Collection<String> values = stocks.values();
-            Iterator<String> iterator = values.iterator();
-
-            while(iterator.hasNext()){
-                String stock = iterator.next();
+            while (totalTask != successed.get()) {
+                String stock = tasks.poll();
                 download(stock);
             }
-            latch.await();
-            retryFailedStock();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -74,7 +67,7 @@ public class SimpleFetcher{
         Runnable downloader = new Runnable() {
             @Override
             public void run() {
-                int byteread = 0;
+                int byteread;
                 try {
                     for (String fileName : files) {
                         URL url = new URL(String.format(BASEURL, stock, fileName));
@@ -92,31 +85,15 @@ public class SimpleFetcher{
                         }
                         fs.close();
                     }
+                    System.out.println("download success:" + stock);
                     successed.incrementAndGet();
                 } catch (Exception e) {
-                    failTasks.add(stock);
+                    tasks.add(stock);
                     System.out.println("股票资料获取失败:" + stock);
-                } finally {
-                    latch.countDown();
                 }
             }
         };
         executor.execute(downloader);
-    }
-
-    private void retryFailedStock(){
-        System.out.println("totel success="+successed);
-        latch = new CountDownLatch(failTasks.toArray().length);
-        while(!failTasks.isEmpty()){
-            String stock = failTasks.poll();
-            System.out.println("尝试重新获取股票资料:"+stock);
-            try {
-                download(stock);
-            } catch (MalformedURLException e) {
-                failTasks.add(stock);
-                System.out.println("股票资料获取失败:" + stock);
-            }
-        }
     }
 
     private Map<String, String> stockList() throws IOException {
@@ -125,7 +102,7 @@ public class SimpleFetcher{
         int count = 0;
         String url = "http://bbs.10jqka.com.cn/codelist.html";
         Document document = Jsoup.connect(url).get();
-        Elements elements = document.select("div[class=bbsilst_wei3] ul a[href~=.(sh|sz).]");
+        Elements elements = document.select("div[class=bbsilst_wei3] ul a[href~=.(sh).]");
         Iterator iter = elements.iterator();
         while (iter.hasNext()) {
             Element e = (Element) iter.next();
@@ -133,13 +110,13 @@ public class SimpleFetcher{
             count++;
             stocks.put(text.substring(0, text.lastIndexOf(" ") - 1), text.substring(text.lastIndexOf(" ")));
         }
-        System.out.println("沪深股票总数="+count);
+        System.out.println("沪深股票总数=" + count);
         return stocks;
     }
 
     public void close() throws InterruptedException {
         System.out.println("程序停止中");
-        while(executor.isShutdown()){
+        while (executor.isShutdown()) {
             executor.shutdown();
             Thread.sleep(1000);
             System.out.print(".");
